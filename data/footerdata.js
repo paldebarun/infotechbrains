6b87f71855a2ac18c8e9c49219587de93d6a7d12
Debarun <paldebarun27@gmail.com>
2024-03-03 23:16:31 +0530

    made footer

---
+++ data/footerdata.js
@@ -0,0 +1,39 @@
+
+export const footerdata={ 
+Globaldata:[
+
+    "Work",
+    "About us",
+    "Blog",
+    "Newsletter",
+    "News",
+    "careers"
+
+],
+
+Technologydata:[
+
+    "Tech",
+    "Open Source"
+
+],
+
+Servicedata:[
+
+    "Product Scope",
+    "UX/UI",
+    "MVP Builder",
+    "Dedicated Teams",
+    "Custom Software Development"
+
+],
+
+Buildata:[
+"Play with Pricing",
+"Book a Call"
+
+]
+
+
+
+}
